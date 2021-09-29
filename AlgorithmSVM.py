7923c5778c04e77a6ce18bb6c073537a14d58a6e
AndresFw <andres.viviescas@uptc.edu.co>
2021-09-29 15:09:00 -0500

    ocurrencia

--- AlgorithmSVM.py
+++ AlgorithmSVM.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 # LECTURA DE ARCHIVO CSV (DATA SET)
 def getSex(x):
@@ -49,3 +50,22 @@ clf = SVC() # instancia el algoritmo
 clf.fit(X_train, y_train) #se entrena
 score = clf.score(X_test, y_test)  # se prueba
 print(score)    
+
+
+#___________________________PASO 2_________________________________________________
+
+x = [i for i in range(100)]
+for i in [0.1, 0.2, 0.3, 0.4]:
+    scores = []
+    for j in x:
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=i) # 20% para test
+        clf = SVC() # instancia el algoritmo
+        clf.fit(X_train, y_train) #se entrena
+        scores.append(clf.score(X_test, y_test))  # se prueba
+
+    plt.plot(x, scores,'bo-')
+    plt.title("Percent Correct: Accuracy of Predictions with "+ str(i*100) +"% of test" )
+    plt.xlabel("Ciclo")
+    plt.ylabel("Percent Correct")
+    plt.show()
+
